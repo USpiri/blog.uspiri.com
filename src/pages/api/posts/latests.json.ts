db169f2ebd854cb8b8c6c880bbce3af01446262d
USpiri <urielspiridione135@gmail.com>
2024-02-14 21:25:34 -0300

    🐛 Fix: Latests posts url

--- src/pages/api/posts/latests.json.ts
+++ src/pages/api/posts/latests.json.ts
@@ -3,10 +3,11 @@ import { getCollection } from "astro:content";
 
 export async function GET({ request }: { request: Request }) {
   const posts = sortPosts(await getCollection("blog"), { limit: 5 });
+  const url = new URL(request.url);
   const data = posts.map((p) => {
     return {
       id: p.id,
-      url: `${request.url.replace("latests.json", "")}${p.slug}`,
+      url: `${url.origin}/blog/${p.slug}`,
       ...p.data,
     };
   });
