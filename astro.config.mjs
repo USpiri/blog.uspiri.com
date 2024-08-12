2fa76f0f0e10eabe72e4502d4b0b82b81b4555bd
USpiri <urielspiridione135@gmail.com>
2024-08-12 15:31:19 -0300

    ✨ Feat: Astro markdown and shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import customTheme from "./src/assets/custom-theme.json";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: customTheme,
+      },
+    },
+  },
 });
+
