f86bface8c54cc87c2a031f5b443aeeab5610352
USpiri <urielspiridione135@gmail.com>
2024-10-14 19:57:58 -0300

    ✨ Feat: Markdown element

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,6 +17,24 @@ const post = defineCollection({
   }),
 });
 
+const project = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    file: z.string(),
+    date: z.date(),
+    cssclasses: z.string().optional(),
+    description: z
+      .string()
+      .max(
+        160,
+        "For best SEO results, please keep the description under 160 characters.",
+      ),
+  }),
+});
+
 export const collections = {
   post,
+  project,
 };
