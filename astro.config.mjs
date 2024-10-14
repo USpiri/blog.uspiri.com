6617bce0daf564a5498ede26abdc8a28c7eeb381
USpiri <urielspiridione135@gmail.com>
2024-10-14 20:00:17 -0300

    ✨ Feat: Katex and render inetgrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import customTheme from "./src/assets/custom-theme.json";
+import markdownIntegration from "@astropub/md";
 
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    markdownIntegration(),
+  ],
   site: "https://blog.uspiri.com",
   base: "/",
   markdown: {
